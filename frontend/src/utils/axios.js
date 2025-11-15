0342328deb3c4b5de3bdad8b827c51fbfe532f73
anuragsharma5259 <anuragsharma03857@gmail.com>
2025-11-16 04:12:26 +0530

    add .env

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,40 +1,7 @@
-import axios from 'axios'
+import axios from 'axios';
 
-// Create axios instance with default config
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
 
-// Request interceptor to add token to every request
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  }
-)
-
-// Response interceptor to handle errors globally
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      // Token expired or invalid
-      localStorage.removeItem('token')
-      localStorage.removeItem('user')
-      window.location.href = '/login'
-    }
-    return Promise.reject(error)
-  }
-)
-
-export default axiosInstance
+export default api;
